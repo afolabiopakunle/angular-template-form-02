0e4455559a4aae3691224c0ead6c540c745a4871
afolabiopakunle <opakunleafolabi@gmail.com>
2021-10-23 21:00:31 +0100

    fetching data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,34 @@
-import { Component, } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit  {
  
   blogTitle = '';
 
   blogPost = '';
-  
+
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.fetchData();
+  }
+
+
   onSubmit(data) {
-    console.warn(data);
+    this.http.post('https://todo-firebase-angular-default-rtdb.firebaseio.com/posts.json', data)
+    .subscribe(response => {
+      console.log(response);
+    })
+  }
+
+  fetchData() {
+    this.http.get('https://todo-firebase-angular-default-rtdb.firebaseio.com/posts.json').pipe()
+    .subscribe(posts => {
+      console.log(posts)
+    })
   }
 }
